0490eb73a1c98771579fd2d267b4bd4a247103f3
Jefferson Huidobro <115659833+OsmarH19@users.noreply.github.com>
2025-11-12 12:50:25 -0500

    Ajuste

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-indigo-600">Hola Tailwind 3.4.17 🎉</h1>
+    </div>
+  );
+}
